59b11be262d4feed197d1309e48282872559524d
Yogesh01000100 <yogeshone678@gmail.com>
2024-05-29 23:42:26 +0530

    feat(controller): add fetch method
    Signed-off-by: Yogesh01000100 <yogeshone678@gmail.com>

--- fabric-samples/backend/backend-service/controllers/hospitalController.js
+++ fabric-samples/backend/backend-service/controllers/hospitalController.js
@@ -244,7 +244,6 @@ export const uploadEHR = async (req, res) => {
     console.log(jsonData);
     // encrypt the data and send to ipfs
 
-    
     const { cid } = await client.add(jsonData);
     console.log(`File uploaded with CID: ${cid}`);
 
@@ -269,6 +268,33 @@ export const uploadEHR = async (req, res) => {
     });
   } catch (error) {
     console.error(`Error: ${error}`);
+    res.status(500).json({ error: error.message });
+  }
+};
+
+export const FetchEHR = async (req, res) => {
+  const channelName = "mychannel";
+  const chaincodeName = "basic";
+  const { userId } = req.body;
+  console.log("Id: ", userId);
+  const gateway = gateways['c84c1dd3-8d65-5ba9-996f-84e9dc9599ae'];
+
+  if (!req.body || Object.keys(req.body).length === 0) {
+    return res.status(400).json({ error: "Request body is empty" });
+  }
+
+  try {
+    const network = await gateway.getNetwork(channelName);
+    const contract = network.getContract(chaincodeName);
+    const result = await contract.evaluateTransaction("FetchEHR", userId);
+    const resultJson = JSON.parse(result.toString());
+    res.json({
+      success: true,
+      message: "Hospital data retrieved successfully!",
+      data: resultJson,
+    });
+  } catch (error) {
+    console.log(`Failed to retrieve data for user ${userId}: ${error}`);
     res.status(500).json({ error: error.message });
   }
 };
